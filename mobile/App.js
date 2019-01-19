0cde109ea008aa59bf17f90994d1a50ec2a1480d
alessandro <alessandro.lages@gmail.com>
2019-01-19 16:33:19 -0200

    testes de local notification e schedule

--- mobile/App.js
+++ mobile/App.js
@@ -7,7 +7,11 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {AppState, Platform, StyleSheet, Text, View, Button} from 'react-native';
+
+import PushNotification from 'react-native-push-notification';
+
+import OneSignal from 'react-native-onesignal';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +20,66 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+//exemplo: https://medium.com/@mstifflin/how-to-set-up-local-notifications-for-android-in-react-native-f062232c4be8
+
+export default class App extends Component {
+
+  componentDidMount(){
+    OneSignal.init('4b02704f-f535-4b1a-bd6e-b07fe0f7daa4');
+
+    AppState.addEventListener('change', this.handleAppStateChange);
+
+  }
+  // This will notify the user in 3 seconds after sending the app to the 
+  // background (like after pressing the home button or switching apps)
+  handleAppStateChange(appState) {
+    if (appState === 'background') {
+      // Schedule a notification
+      var date = new Date();
+
+      PushNotification.localNotificationSchedule({
+        title : 'Titulo da notificacao agendada em backgound',
+        message: 'Notificacao local agendada '+ date.toLocaleTimeString(), // (required)
+        date: new Date(Date.now() + (10 * 1000)) // in 3 secs,
+      });
+
+/*
+      var agend = new Date();
+      agend.setHours(16, 10, 0);
+
+      PushNotification.localNotificationSchedule({
+        title : 'Notificacao as 16:10',
+        message: 'devera rodar as 16:10 '+ agend.toLocaleTimeString(), // (required)
+        date: agend
+      });
+    }
+    */
+  };
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
+  };
+
+
+  sendLocalNotification() {
+    var date = new Date();
+
+    PushNotification.localNotification({
+      title : 'Titulo da notificacao pelo botao',
+      message: 'Notificacao disparada pelo botao '+ date.toLocaleTimeString()
+    });
+  };
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Teste de Notificacao</Text>
+
+
+        <Button title='Clique para notificar agora'
+          onPress={this.sendLocalNotification} />
+
       </View>
     );
   }
@@ -40,10 +96,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
--- mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -2,7 +2,11 @@ package com.mobile;
 
 import android.app.Application;
 
+
+
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
+            new ReactNativeOneSignalPackage()
       );
     }
 
